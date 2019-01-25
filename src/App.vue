2a4d05c9eefce23236fa34948d97ab13e01b7df6
Sam <axbytez@gmail.com>
2019-01-25 18:49:44 +0100

    init
    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
+
+body {
+  background-color: $dark-blue;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: $system-font;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: white;
 }
 #nav {
   padding: 30px;
